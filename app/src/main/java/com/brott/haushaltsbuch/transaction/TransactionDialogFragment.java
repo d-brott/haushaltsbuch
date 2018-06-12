c30085baad86c0337103f46c82409df28151faa8
David Brott <david.brott@outlook.de>
2018-06-12 18:29:26 +0200

    design and functionality of transaction dialog changed and extendend

--- app/src/main/java/com/brott/haushaltsbuch/transaction/TransactionDialogFragment.java
+++ app/src/main/java/com/brott/haushaltsbuch/transaction/TransactionDialogFragment.java
@@ -16,23 +16,23 @@ import com.brott.haushaltsbuch.R;
 import com.brott.haushaltsbuch.account.AccountNameEnum;
 import com.brott.haushaltsbuch.account.AccountTypeEnum;
 import com.brott.haushaltsbuch.data.Transaction;
+import com.brott.haushaltsbuch.utilities.HelperMethods;
 
 public class TransactionDialogFragment extends DialogFragment {
 
     private TransactionViewModel transactionViewModel;
 
     private Spinner categorySpinner;
+    private Spinner accountTypeSpinner;
     private EditText amountEditTxt;
     private EditText noteEditTxt;
 
     private Button saveBtn;
-    private Button moneyBtn;
-    private Button checkAccountBtn;
-    private Button savingsBtn;
-    private Button portfolioBtn;
+    private Button cancelBtn;
 
-    private String accountName = "";
-    private String note = "";
+    private String selectedAccount;
+    private String selectedCategory;
+    private String note;
     private double amount;
 
     @Override
@@ -48,43 +48,53 @@ public class TransactionDialogFragment extends DialogFragment {
         amountEditTxt = root.findViewById(R.id.amount_edit_text);
         noteEditTxt = root.findViewById(R.id.note_edit_text);
         saveBtn = root.findViewById(R.id.save_button);
-        moneyBtn = root.findViewById(R.id.money_btn);
-        checkAccountBtn = root.findViewById(R.id.check_account_btn);
-        savingsBtn = root.findViewById(R.id.savings_btn);
-        portfolioBtn = root.findViewById(R.id.portfolio_btn);
+        cancelBtn = root.findViewById(R.id.cancel_button);
+        accountTypeSpinner = root.findViewById(R.id.account_type_spinner);
 
         ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(getContext(),
                 R.array.categories, android.R.layout.simple_spinner_item);
         adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
         categorySpinner.setAdapter(adapter);
 
+        ArrayAdapter<CharSequence> accountTypeAdapter = ArrayAdapter.createFromResource(getContext(),
+                R.array.accountType, android.R.layout.simple_spinner_item);
+        accountTypeAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        accountTypeSpinner.setAdapter(accountTypeAdapter);
+
         saveBtn.setOnClickListener(e -> {
+            if (HelperMethods.isEmpty(amountEditTxt)) {
+                amountEditTxt.setError(getResources().getString(R.string.ungueltigerBetrag));
+                return;
 
-            amount = Double.parseDouble(amountEditTxt.getText().toString());
-            note = noteEditTxt.getText().toString();
+            } else {
+                if (HelperMethods.isEmpty(noteEditTxt)) {
+                    note = "";
+                } else {
+                    note = noteEditTxt.getText().toString().trim();
+                }
 
-            Transaction transaction = new Transaction(accountName, AccountTypeEnum.ACCOUNT_FOR_PAYMENTS.toString(), "Lebensmittel", note, amount, "30.05.2018");
+                amount = Double.parseDouble(amountEditTxt.getText().toString().trim());
 
-            transactionViewModel = ViewModelProvider.AndroidViewModelFactory.getInstance(getActivity().getApplication()).create(TransactionViewModel.class);
-            transactionViewModel.insert(transaction);
+                selectedAccount = accountTypeSpinner.getSelectedItem().toString();
+                selectedCategory = categorySpinner.getSelectedItem().toString();
 
+                Transaction transaction = new Transaction(
+                        selectedAccount,
+                        HelperMethods.determineAccountType(selectedAccount),
+                        selectedCategory,
+                        note,
+                        amount,
+                        "30.05.2018");
+
+                transactionViewModel = ViewModelProvider.AndroidViewModelFactory.getInstance(getActivity().getApplication()).create(TransactionViewModel.class);
+                transactionViewModel.insert(transaction);
+
+                getDialog().dismiss();
+            }
+        });
+
+        cancelBtn.setOnClickListener(e -> {
             getDialog().dismiss();
-        });
-
-        moneyBtn.setOnClickListener(e -> {
-            accountName = AccountNameEnum.MONEY.toString();
-        });
-
-        savingsBtn.setOnClickListener(e -> {
-            accountName = AccountNameEnum.SAVINGS_ACCOUNT.toString();
-        });
-
-        portfolioBtn.setOnClickListener(e -> {
-            accountName = AccountNameEnum.PORTFOLIO.toString();
-        });
-
-        checkAccountBtn.setOnClickListener(e -> {
-            accountName = AccountNameEnum.CHECK_ACCOUNT.toString();
         });
 
         return root;
--- app/src/main/java/com/brott/haushaltsbuch/utilities/HelperMethods.java
+++ app/src/main/java/com/brott/haushaltsbuch/utilities/HelperMethods.java
@@ -1,5 +1,10 @@
 package com.brott.haushaltsbuch.utilities;
 
+import android.widget.EditText;
+
+import com.brott.haushaltsbuch.account.AccountNameEnum;
+import com.brott.haushaltsbuch.account.AccountTypeEnum;
+
 import java.text.NumberFormat;
 import java.util.Locale;
 
@@ -18,5 +23,30 @@ public class HelperMethods {
         String currency = currencyFormatter.format(amount);
 
         return currency;
+    }
+
+    public static boolean isEmpty(EditText editText) {
+        String input = editText.getText().toString().trim();
+        return input.length() == 0;
+    }
+
+    public static String determineAccountType(String accountName) {
+        String accountType = "";
+
+        if (accountName.equals(AccountNameEnum.MONEY.toString())) {
+            accountType = AccountTypeEnum.ACCOUNT_FOR_PAYMENTS.toString();
+
+        } else if (accountName.equals(AccountNameEnum.CHECK_ACCOUNT.toString())) {
+            accountType = AccountTypeEnum.ACCOUNT_FOR_PAYMENTS.toString();
+
+        } else if (accountName.equals(AccountNameEnum.SAVINGS_ACCOUNT.toString())) {
+            accountType = AccountTypeEnum.SAVINGS_ACCOUNT.toString();
+
+        } else if (accountName.equals(AccountNameEnum.PORTFOLIO.toString())) {
+            accountType = AccountTypeEnum.SAVINGS_ACCOUNT.toString();
+
+        }
+
+        return accountType;
     }
 }
